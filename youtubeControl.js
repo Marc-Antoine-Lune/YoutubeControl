f0cdd2a17a5da5653bcd0c29d9ecd961837cd6f2
Marc-Antoine-Lune <luneschima@gmail.com>
2018-08-16 14:53:08 +0200

    Premiere sauvegarde (Test git/gitHub)

---
+++ youtubeControl.js
@@ -0,0 +1,58 @@
+
+	var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+	var recognition = new SpeechRecognition();
+	recognition.continuous = true;
+	recognition.lang="fr-FR";
+	recognition.onresult = function(event){
+
+		
+		for(var i=event.resultIndex; i<event.results.length; i++){
+			var test = event.results[i][0].transcript;
+			document.getElementById("rechercheVocale").value = test;
+
+		}
+
+	}
+
+	
+
+
+function demarer(){
+	recognition.start();
+
+	
+	
+
+	setTimeout("recupYoutube()", 5000);
+	
+		
+}
+
+
+
+function recupYoutube(){
+		var x = document.getElementById("rechercheVocale");
+		var request = new XMLHttpRequest();
+		var recherche = x.value;
+		var url = "https://www.googleapis.com/youtube/v3/search?&q=" + recherche + "&part=snippet&key=" + "AIzaSyD-9VMPpURE4Ng992hjjr20hlDA35kDWlo";
+			
+			request.open("GET", url);
+			request.send();
+			request.responseType = 'json';
+		
+
+			request.onload = function(){
+				var tab = request.response;
+				document.getElementById("listeVideos").innerHTML = "";
+
+				for(var i=0; i<5; i++){
+					var titre = tab['items'][i]['snippet']['title'];
+					var urlVideo= tab['items'][1]['id']['videoId'];
+
+					document.getElementById("listeVideos").innerHTML += "<li><a href=https://www.youtube.com/watch?v=" + urlVideo + ">" + titre + "</a></li>";
+				}
+			}
+
+
+}
